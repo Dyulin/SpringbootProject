b18a362eddf8ed38c26b39a812fdf7f565e37b99
Dyulin <40668726+Dyulin@users.noreply.github.com>
2019-02-26 23:50:32 +0800

    update 2019.2.26 123

--- src/com/example01/HelloWorld.java
+++ src/com/example01/HelloWorld.java
@@ -3,6 +3,6 @@ package com.example01;
 public class HelloWorld {
     public static  void  main(String[] args)
     {
-        System.out.println("123");
+        System.out.println("1234");
     }
 }
